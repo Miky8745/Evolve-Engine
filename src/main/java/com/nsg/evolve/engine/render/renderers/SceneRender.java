1dbc69f26fe31e2a98e3ac3ddb29145885146e9e
BuildTools <mikulas.vanecek@gmail.com>
2024-10-11 20:34:15 +0200

    Removed redundant light update method and imports

--- src/main/java/com/nsg/evolve/engine/render/renderers/SceneRender.java
+++ src/main/java/com/nsg/evolve/engine/render/renderers/SceneRender.java
@@ -11,14 +11,6 @@ import com.nsg.evolve.engine.render.shaders.Shaders;
 import com.nsg.evolve.engine.render.shaders.Uniforms;
 import com.nsg.evolve.engine.scene.Scene;
 import com.nsg.evolve.engine.scene.animations.AnimationData;
-import com.nsg.evolve.engine.scene.lighting.SceneLights;
-import com.nsg.evolve.engine.scene.lighting.lights.AmbientLight;
-import com.nsg.evolve.engine.scene.lighting.lights.DirectionalLight;
-import com.nsg.evolve.engine.scene.lighting.lights.PointLight;
-import com.nsg.evolve.engine.scene.lighting.lights.SpotLight;
-import org.joml.Matrix4f;
-import org.joml.Vector3f;
-import org.joml.Vector4f;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -116,91 +108,5 @@ public class SceneRender {
         glBindVertexArray(0);
         glEnable(GL_BLEND);
         shaderProgram.unbind();
-    }
-
-    private void updateLights(Scene scene) {
-        Matrix4f viewMatrix = scene.getCamera().getViewMatrix();
-
-        SceneLights sceneLights = scene.getSceneLights();
-        AmbientLight ambientLight = sceneLights.getAmbientLight();
-        uniformsMap.setUniform("ambientLight.factor", ambientLight.getIntensity());
-        uniformsMap.setUniform("ambientLight.color", ambientLight.getColor());
-
-        DirectionalLight dirLight = sceneLights.getDirLight();
-        Vector4f auxDir = new Vector4f(dirLight.getDirection(), 0);
-        auxDir.mul(viewMatrix);
-        Vector3f dir = new Vector3f(auxDir.x, auxDir.y, auxDir.z);
-        uniformsMap.setUniform("dirLight.color", dirLight.getColor());
-        uniformsMap.setUniform("dirLight.direction", dir);
-        uniformsMap.setUniform("dirLight.intensity", dirLight.getIntensity());
-
-        List<PointLight> pointLights = sceneLights.getPointLights();
-        int numPointLights = pointLights.size();
-        PointLight pointLight;
-        for (int i = 0; i < MAX_POINT_LIGHTS; i++) {
-            if (i < numPointLights) {
-                pointLight = pointLights.get(i);
-            } else {
-                pointLight = null;
-            }
-            String name = "pointLights[" + i + "]";
-            updatePointLight(pointLight, name, viewMatrix);
-        }
-
-
-        List<SpotLight> spotLights = sceneLights.getSpotLights();
-        int numSpotLights = spotLights.size();
-        SpotLight spotLight;
-        for (int i = 0; i < MAX_SPOT_LIGHTS; i++) {
-            if (i < numSpotLights) {
-                spotLight = spotLights.get(i);
-            } else {
-                spotLight = null;
-            }
-            String name = "spotLights[" + i + "]";
-            updateSpotLight(spotLight, name, viewMatrix);
-        }
-    }
-
-    private void updatePointLight(PointLight pointLight, String prefix, Matrix4f viewMatrix) {
-        Vector4f aux = new Vector4f();
-        Vector3f lightPosition = new Vector3f();
-        Vector3f color = new Vector3f();
-        float intensity = 0.0f;
-        float constant = 0.0f;
-        float linear = 0.0f;
-        float exponent = 0.0f;
-        if (pointLight != null) {
-            aux.set(pointLight.getPosition(), 1);
-            aux.mul(viewMatrix);
-            lightPosition.set(aux.x, aux.y, aux.z);
-            color.set(pointLight.getColor());
-            intensity = pointLight.getIntensity();
-            PointLight.Attenuation attenuation = pointLight.getAttenuation();
-            constant = attenuation.getConstant();
-            linear = attenuation.getLinear();
-            exponent = attenuation.getExponent();
-        }
-        uniformsMap.setUniform(prefix + ".position", lightPosition);
-        uniformsMap.setUniform(prefix + ".color", color);
-        uniformsMap.setUniform(prefix + ".intensity", intensity);
-        uniformsMap.setUniform(prefix + ".att.constant", constant);
-        uniformsMap.setUniform(prefix + ".att.linear", linear);
-        uniformsMap.setUniform(prefix + ".att.exponent", exponent);
-    }
-
-    private void updateSpotLight(SpotLight spotLight, String prefix, Matrix4f viewMatrix) {
-        PointLight pointLight = null;
-        Vector3f coneDirection = new Vector3f();
-        float cutoff = 0.0f;
-        if (spotLight != null) {
-            coneDirection = spotLight.getConeDirection();
-            cutoff = spotLight.getCutOff();
-            pointLight = spotLight.getPointLight();
-        }
-
-        uniformsMap.setUniform(prefix + ".conedir", coneDirection);
-        uniformsMap.setUniform(prefix + ".cutoff", cutoff);
-        updatePointLight(pointLight, prefix + ".pl", viewMatrix);
     }
 }
